4bdd3dfae39e1a7418e2c079d8c7bce8d146be50
pupaufa <aufadhiaulhaq25@gmail.com>
2026-08-26 22:15:30 +0700

    Add files via upload

---
+++ AufaProjek1.java
@@ -0,0 +1,13 @@
+package aufaprojek1;
+
+public class AufaProjek1 {
+
+    public static void main(String[] args) {
+       System.out.println("Nama  : Aufa ");
+       System.out.println("NPM   : 2505060017 ");
+       System.out.println("Alamat: Magelang ");
+       System.out.println("No_HP : 081234567890");
+    }
+    
+    
+}
